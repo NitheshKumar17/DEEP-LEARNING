5ebe642f07736d8a92a09b24bdfe046d998af7a7
NitheshKumar17 <231501114@rajalakshmi.edu.in>
2025-11-02 23:54:54 +0530

    Add Face_Antispoofing_System project folder

---
+++ camera_test.py
@@ -0,0 +1,14 @@
+import cv2
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+if not cap.isOpened():
+    print("Camera not opened")
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("No frame")
+        break
+    cv2.imshow("Test", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
